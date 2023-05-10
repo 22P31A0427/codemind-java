b2006410d6fcdc9c4ce1d789767ef2a02e7a5e3e
22P31A0427 <119720707+22P31A0427@users.noreply.github.com>
2023-05-10 19:35:05 +0530

    import java.util.*; public class solution {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         double a,b;         double c;         a=sc.nextDouble();         b=sc.nextDouble();         c=a*b;         System.out.format("%.2f",c);     }
    }

---
+++ Program_to_Multiply_Two_Floating-Point_Numbers.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class solution
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        double a,b;
+        double c;
+        a=sc.nextDouble();
+        b=sc.nextDouble();
+        c=a*b;
+        System.out.format("%.2f",c);
+    }
+    
+}
